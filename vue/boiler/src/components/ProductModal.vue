3a2bdc05178425aaeb13f61acf095027f1af8f75
ArSo77 <arsobieraj@gmail.com>
2020-04-25 12:55:04 +0200

    add vualidate

---
+++ vue/boiler/src/components/ProductModal.vue
@@ -0,0 +1,17 @@
+<template>
+    <modal>
+
+    </modal>
+</template>
+
+<script>
+    import Modal from "./Modal";
+    export default {
+        name: "ProductModal",
+        components: {Modal}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vue/boiler/src/main.js
+++ vue/boiler/src/main.js
@@ -2,8 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuelidate from "vuelidate/src";
+
 
 Vue.config.productionTip = false
+
+Vue.directive('click-outside', {
+  bind (el, binding) {
+    el.__ClickOutsideHandler__ = evt => {
+      if (!(el === evt.target || el.contains(evt.target))) {
+        binding.value(evt);
+      }
+    };
+    document.body.addEventListener('click', el.__ClickOutsideHandler__)
+  },
+  unbind (el) {
+    document.body.removeEventListener('click', el.__ClickOutsideHandler__)
+  }
+});
+
+Vue.use(Vuelidate);
+
+
 
 new Vue({
   router,
