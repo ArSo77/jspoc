0d6fb15144309ca39d5c57e7962f78d2ab048dc5
asobieraj <asobieraj@unitdoseone.com>
2020-05-21 23:23:33 +0200

    add carrying

---
+++ js/carrying.js
@@ -0,0 +1,40 @@
+// Problem
+function multiply(value, multiplier) {
+    return value * multiplier
+}
+
+const numbers = [44, 3, 84, 23];
+
+// numbers.map(multiply(2));
+
+
+// Partial Application
+const double = multiply.bind(null , 2);
+double(5);
+console.log(numbers.map(double))
+
+
+//Unary & binary
+const unary = (x) => console.log(x);
+const binary = (x, y) => x + y;
+
+const binToUnrFunc = function (x) {
+    return function (y) {
+        return x + y;
+    }
+}
+
+//Curring
+const unaryCurring = (x) => (y) => x + y;
+unaryCurring(1);
+unaryCurring(1)(2);
+
+//Ramda
+
+
+
+
+
+
+
+
